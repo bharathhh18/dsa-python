451a70344be0c83d4a0345428d2b45172426c48f
bharathhh18 <sts695112@gmail.com>
2026-07-22 22:03:33 +0530

    Done a recursion question

--- Recursion/AdvanceRecursion.py
+++ Recursion/AdvanceRecursion.py
@@ -207,3 +207,27 @@ def func(index,total,subset):
 func(1,0,[])
 print(result)
 
+#Letter combination of a phone number
+char_set={
+"2":"abc",
+"3":"def",
+"4":"ghi",
+"5":"jkl",
+"6":"mno",
+"7":"pqrs",
+"8":"tuv",
+"9":"wxyz"
+}
+result=[]
+digits=str(input("Enter the digit:"))
+def func(index,subset):
+    if index>=len(digits):
+        result.append("".join(subset))
+        return
+    for ch in char_set[digits[index]]:
+        subset.append(ch)
+        func(index+1,subset)
+        subset.pop()
+func(0,[])
+print(result)
+
