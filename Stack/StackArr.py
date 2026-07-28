b9e62a0f7d811e2a84a08915533b337bd6190541
bharathhh18 <sts695112@gmail.com>
2026-07-28 22:08:25 +0530

    Stack using queue

--- Stack/StackArr.py
+++ Stack/StackArr.py
@@ -29,3 +29,44 @@ stack.display()
 print(stack.topmost())
 print(stack.popp())
 stack.display()
+
+#stack using queue
+from collections import deque
+
+class StackusingQueue:
+
+    def __init__(self):
+        self.queue=deque()
+
+    def push(self,item):
+        self.queue.append(item)
+        for _ in range(len(self.queue)-1):
+            self.queue.append(self.queue.popleft())
+
+    def pop(self):
+        if len(self.queue)==0:
+            return "Stack is empty"
+        return self.queue.popleft()
+
+    def top(self):
+        if len(self.queue)==0:
+            return "Stack is empty"
+        return self.queue[0]
+
+    def isEmpty(self):
+        if len(self.queue)==0:
+            return True
+        return False
+    
+    def display(self):
+        for num in self.queue:
+            print(num)
+
+q=StackusingQueue()
+
+q.push(120)
+q.push(90)
+q.display()
+q.pop()
+print()
+q.display()
